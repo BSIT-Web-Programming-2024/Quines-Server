478e5d7e7734a31ba7a8cf32caf6cebcffc1eba9
Quines Jhona <jhonav402@gmail.com>
2024-12-04 09:00:16 +0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -6,6 +6,6 @@ $pass="";
 $db="login";
 $conn=new mysqli($host,$user,$pass,$db);
 if($conn->connect_error){
-    echo "Failed to connect DB".$conn->_error;
+    echo "Failed to connect DB".$conn->connect_error;
 }
 ?>
